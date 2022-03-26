c1569bf5b6f2c65cfabcbee3c757cde591524eeb
sahil78272026 <sahil.garg78272026@gmail.com>
2022-03-26 21:01:42 +0530

    Add files via upload

---
+++ self_practice1.py
@@ -0,0 +1,13 @@
+user_input = input("Do you want to play a game? ")
+
+if (user_input!="yes"):
+    exit()
+
+
+print('''Okay....First Introduce Yourself ...''')
+
+user_name = input("Whats your Name ?")
+print("Hello " , user_name)
+
+user_location = input("Where do you live ?")
+print(user_location , "...Cool , that's a beautiful area")
